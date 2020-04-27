b8f3afb28a7bffd8a158c537ad452b2b0ad502bf
Lee Garner <lee@leegarner.com>
2020-04-27 09:12:52 -0700

    Bump glFusion version to match plugin.xml

--- membership.php
+++ membership.php
@@ -1,26 +1,26 @@
 <?php
 /**
-*   Table definitions and other static config variables.
-*
-*   @author     Lee Garner <lee@leegarner.com>
-*   @copyright  Copyright (c) 2012-2017 Lee Garner <lee@leegarner.com>
-*   @package    membership
-*   @version    0.1.2
-*   @license    http://opensource.org/licenses/gpl-2.0.php 
-*               GNU Public License v2 or later
-*   @filesource
-*/
+ * Table definitions and other static config variables.
+ *
+ * @author      Lee Garner <lee@leegarner.com>
+ * @copyright   Copyright (c) 2012-2020 Lee Garner <lee@leegarner.com>
+ * @package     membership
+ * @version     v0.2.0
+ * @license     http://opensource.org/licenses/gpl-2.0.php
+ *              GNU Public License v2 or later
+ * @filesource
+ */
 
 /**
-*   Global array of table names from glFusion
-*   @global array $_TABLES
-*/
+ * Global array of table names from glFusion.
+ * @global array $_TABLES
+ */
 global $_TABLES;
 
 /**
-*   Global table name prefix
-*   @global string $_DB_table_prefix
-*/
+ * Global table name prefix.
+ * @global string $_DB_table_prefix
+ */
 global $_DB_table_prefix;
 $prefix = $_DB_table_prefix . 'membership_';
 $_TABLES['membership_members']  = $prefix . 'members';
@@ -33,18 +33,18 @@ $_TABLES['membership_positions'] = $prefix . 'positions';
 $_TABLES['membership_users']    = $prefix . 'users';
 
 /**
-*   Global configuration array
-*   @global array $_CONF_MEMBERSHIP
-*/
+ * Global configuration array.
+ * @global array $_CONF_MEMBERSHIP
+ */
 global $_CONF_MEMBERSHIP;
 $_CONF_MEMBERSHIP['pi_name']           = 'membership';
 $_CONF_MEMBERSHIP['pi_version']        = '0.2.0';
-$_CONF_MEMBERSHIP['gl_version']        = '1.6.0';
+$_CONF_MEMBERSHIP['gl_version']        = '1.7.0';
 $_CONF_MEMBERSHIP['pi_url']            = 'http://www.leegarner.com';
 $_CONF_MEMBERSHIP['pi_display_name']   = 'Membership';
 
 $_CONF_MEMBERSHIP['icons'] = array(
-    'delete'    => '<i class="uk-icon uk-icon-trash-o uk-text-danger"></i>',
+    'delete'    => '<i class="uk-icon uk-icon-remove uk-text-danger"></i>',
     'copy'      => '<i class="uk-icon uk-icon-clone"></i>',
     'edit'      => '<i class="uk-icon uk-icon-edit"></i>',
     'arrow-up'  => '<i class="uk-icon uk-icon-arrow-up"></i>',
